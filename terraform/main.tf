aafc4e792a62fa8ec108750da315584362de19a8
gunzrun <d.almuhametov@gmail.com>
2019-06-30 15:59:52 +0300

    dfadf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,21 @@
 provider "google" {
     version = "~> 2.5"
-    project = "ace-falcon-237616"
-    region  = "us-east1-b"
+    project = "${var.project}"
+    region  = "${var.region}"
 }
 resource "google_compute_instance" "app" {
     metadata = {
-        ssh-keys = "den:${file("~/.ssh/id_rsa.pub")}"
+        ssh-keys = "den:${file(var.public_key_path)}"
     }
     name = "reddit-app"
     machine_type = "g1-small"
     zone = "europe-west1-b"
     tags = ["reddit-app"]
-
+    
     # определение загрузочного диска
     boot_disk {
         initialize_params {
-            image = "reddit-base"
+            image = "${var.disk_image}"
         }
     }
     
@@ -25,6 +25,14 @@ resource "google_compute_instance" "app" {
         network = "default"
         # использовать ephemeral IP для доступа из Интернет
         access_config {}
+    }
+    
+    connection {
+        host = "${google_compute_instance.app.network_interface.0.access_config.0.nat_ip}"
+        type = "ssh"
+        user = "den"
+        agent = false
+        private_key = "${file("~/.ssh/id_rsa")}"
     }
 
     provisioner "file" {
@@ -50,4 +58,17 @@ resource "google_compute_firewall" "firewall_puma" {
     source_ranges = ["0.0.0.0/0"]
     # Правило применимо для инстансов с тегом ...
     target_tags = ["reddit-app"]
+}
+
+resource "google_compute_firewall" "firewall_ssh" {
+    name    = "default-allow-ssh"
+    # Название сети, в которой действует правило
+    network = "default"
+    # Какой доступ разрешить
+    allow { 
+        protocol = "tcp"
+        ports    = ["22"]
+    }
+    # Каким адресам разрешаем доступ
+    source_ranges = ["0.0.0.0/0"]
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable project {
+    description = "Project ID"
+}
+variable region {
+    description = "Region"
+    default = "europe-west1"
+}
+variable public_key_path {
+    description = "Path to the public key for ssh access" 
+}
+variable disk_image {
+    description = "Disk image"
+}
+
